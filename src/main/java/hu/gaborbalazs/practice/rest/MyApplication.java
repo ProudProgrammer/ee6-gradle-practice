26ead050f6701bdbdf005721de95b612e7cf4e58
Balázs Gábor <mail.gabor.balazs@gmail.com>
2017-08-20 15:53:27 +0200

    REST introduce

---
+++ src/main/java/hu/gaborbalazs/practice/rest/MyApplication.java
@@ -0,0 +1,9 @@
+package hu.gaborbalazs.practice.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("")
+public class MyApplication extends Application {
+
+}
