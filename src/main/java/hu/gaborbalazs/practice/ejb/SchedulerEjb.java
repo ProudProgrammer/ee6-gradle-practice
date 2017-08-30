a1bc90113f571a372b7bae37485c2f937ba2ae19
gbalazs <gbalazs@localhost>
2017-08-30 16:38:59 +0200

    First use of @Schedule

---
+++ src/main/java/hu/gaborbalazs/practice/ejb/SchedulerEjb.java
@@ -0,0 +1,19 @@
+package hu.gaborbalazs.practice.ejb;
+
+import javax.ejb.Schedule;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+
+@Stateless
+public class SchedulerEjb {
+
+    @Inject
+    private Logger logger;
+
+    @Schedule(second = "*/10", minute = "*", hour = "*")
+    public void logScheduler() {
+        logger.trace("I am alive");
+    }
+}
